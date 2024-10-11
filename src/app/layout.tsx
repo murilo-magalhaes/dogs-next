be5d377e2ad2724d65da134e84cdc9c8f3e5b84d
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-11 09:31:32 -0300

    feat: footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { type_second } from './functions/fonts';
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
@@ -14,9 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${type_second.variable}`}>
-        <Header />
-        {children}
+      <body className={type_second.variable}>
+        <div className="App">
+          <Header />
+          <main className="AppBody">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );

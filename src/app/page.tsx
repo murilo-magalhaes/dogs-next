41acd4e463b928c58186b51314743fb108aaf4e5
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-11 09:57:42 -0300

    feat: feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import photosGet from '@/actions/photos-get';
+import Feed from '@/components/feed/feed';
+
+export default async function Home() {
+  const photos = await photosGet();
+
   return (
     <section className="container mainContainer">
       <h1 className="title">Dogs Next</h1>
+      <Feed photos={photos} />
     </section>
   );
 }

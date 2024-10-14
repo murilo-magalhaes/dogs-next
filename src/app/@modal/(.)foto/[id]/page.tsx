6c15be7a3e71e6c1c6bc68674e24d5328d96fb5c
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-14 18:04:33 -0300

    feat: photo modal

---
+++ src/app/@modal/(.)foto/[id]/page.tsx
@@ -0,0 +1,33 @@
+import photoGet from '@/actions/photo-get';
+import FeedModal from '@/components/feed/feed-modal';
+import PhotoContent from '@/components/photo/photo-content';
+import { Metadata } from 'next';
+
+type FotoIdParams = {
+  params: {
+    id: string;
+  };
+};
+
+export async function generateMetadata({
+  params,
+}: FotoIdParams): Promise<Metadata | undefined> {
+  const { data } = await photoGet(params.id);
+
+  if (!data) return { title: 'Fotos' };
+
+  return {
+    title: data.photo.title + ' | Dogs Next',
+  };
+}
+
+export default async function FotoIdPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { data } = await photoGet(params.id);
+  if (!data) return null;
+
+  return <FeedModal photo={data} />;
+}

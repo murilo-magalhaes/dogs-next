a69351fffff77df6579d5f16a29b97ff2b8bec3f
murilo-magalhaes <murilomagalhaes60@gmail.com>
2024-10-14 18:08:59 -0300

    feat: profile

--- src/app/perfil/[user]/page.tsx
+++ src/app/perfil/[user]/page.tsx
@@ -1,4 +1,19 @@
-export default async function PerfilUserPage({params}: {params: {user: string}}) {
+import photosGet from '@/actions/photos-get';
+import Feed from '@/components/feed/feed';
 
-  return <main><h1>Usuário {params.user}</h1></main>;
+export default async function PerfilUserPage({
+  params,
+}: {
+  params: { user: string };
+}) {
+  const { data } = await photosGet({ user: params.user });
+
+  if (!data) return null;
+
+  return (
+    <section className="container mainSection">
+      <h1 className="title">{params.user}</h1>
+      <Feed photos={data} user={params.user} />
+    </section>
+  );
 }
